910a179286ed49c6ca75610fcaa5be21f71f324c
nixson <admin@nixson.ru>
2016-08-19 12:40:01 +0300

    auto

--- qt/image/wsworker.cpp
+++ qt/image/wsworker.cpp
@@ -108,8 +108,11 @@ void WsWorker::processTextMessage(QString message){
                     connect(wtask,&WsTask::response,this,&WsWorker::response);
                     QThreadPool::globalInstance()->start(wtask);
                     ++user[idusersocs].tasks;
+                    break;
                 }
+                break;
             }
+            break;
         }
         std::cout << "QThreadPool start: " << user[idusersocs].tasks << std::endl;
     }
