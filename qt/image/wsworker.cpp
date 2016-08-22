dad78cd9680273c4b233336e8dd4ae17708ae130
nixson <admin@nixson.ru>
2016-08-22 13:02:54 +0300

    auto

--- qt/image/wsworker.cpp
+++ qt/image/wsworker.cpp
@@ -151,7 +151,7 @@ void WsWorker::response(iTask *step, iTaskResult *result){
         if(find){
             QDateTime current = QDateTime::currentDateTime();
             uint currentInt = current.toTime_t() - goodStep->PeriodStart;
-            QString msg = QString::number(currentInt)+":"+QString::number(goodStep->perc)+":"+QString::number(goodStep->rate);
+            QString msg = "{\"dtime\":"+QString::number(currentInt)+",\"perc\":"+QString::number(goodStep->perc)+",\"rate\":"+QString::number(goodStep->rate)+"}";
             am_clients[step->iduser]->sendTextMessage(msg);
         }
         user[step->iduser].tasks = 0;
